cd49a34ff7519086790c2ee5ff807ee80e406d5f
Filip Tomes <filiptomes@protonmail.com>
2024-08-01 17:26:28 +0200

    style: color changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       colors: {
-        beige: "#FFF1E1",
-        brown: "#EFB479",
-        "dark-grey": "#1E1E1E",
+        beige: '#FFF1E1',
+        brown: '#EFB479',
+        'gray-dark': '#1E1E1E',
       },
       boxShadow: {
-        solid: "6px 7px 0px 0px rgba(0,0,0,0.75)",
+        'solid-gray': '6px 7px 0px 0px rgba(30,30,30,1)',
+        'solid-black': '6px 7px 0px 0px rgba(0,0,0,1)',
       },
     },
   },
